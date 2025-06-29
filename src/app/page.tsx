ba95e6dd8781c4798281c0cc4720c0228627a4c5
Worakrit Kullanatpokin <worrakit.boss@gmail.com>
2025-06-30 01:44:03 +0700

    fix: fix problem.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,21 @@ import Qoute from "@/components/landing/Qoute";
 export default function Home() {
   return (
     <div className="!pb-(--space-4xl) px-(--space-m) py-(--space-s) max-w-[1728px] w-full flex justify-self-center flex-col justify-center items-center gap-y-(--space-4xl)">
-      <div className="flex flex-col md:flex-row gap-(--space-l) justify-between mt-(--space-m)">
-        <div className="inline-block md:justify-start mx-auto text-center">
-          <ScrambleText className="xl:text-[300px] md:text-[20vw] text-[30vw] font-bold text-green no_line_height w-fit ">
-            <span className="glow glow-green-lg">SOS14</span>
-          </ScrambleText>
+      <section className="flex-col-reverse">
+        <div className="flex flex-col md:flex-row gap-(--space-l) justify-between mt-(--space-m)">
+          <div className="inline-block md:justify-start mx-auto text-center">
+            <ScrambleText className="xl:text-[300px] md:text-[20vw] text-[30vw] font-bold text-green no_line_height w-fit ">
+              <span className="glow glow-green-lg">SOS14</span>
+            </ScrambleText>
+          </div>
+          <div className="flex items-center">
+            <p className="text-1 md:text-left text-center text-white/60">
+              Welcome to the 14th SOS camp, a preparation camp for SKE23
+              students. We will take you through Python programming fundamentals
+            </p>
+          </div>
         </div>
-        <div className="flex items-center">
-          <p className="text-1 md:text-left text-center text-white/60">
-            Welcome to the 14th SOS camp, a preparation camp for SKE23 students.
-            We will take you through Python programming fundamentals
-          </p>
-        </div>
-      </div>
+      </section>
 
       <section>
         <h3>What is SOS camp?</h3>
--- src/app/problems/page.tsx
+++ src/app/problems/page.tsx
@@ -1,6 +1,7 @@
 import ScrambleText from "@/components/effects/ScrambleText";
 import ProgressBar from "@/components/problems/ProgressBar";
 import ProblemList from "@/components/problems/ProblemList";
+import Link from "next/link";
 
 export default function Problems() {
   return (
@@ -8,15 +9,19 @@ export default function Problems() {
       <ProgressBar />
       <div className="!pb-(--space-4xl) px-(--space-m) py-(--space-s) max-w-[1728px] w-full flex flex-col justify-self-center justify-center">
         <div className="flex flex-col my-(--space-l) gap-(--space-s) items-center">
-          <ScrambleText className="text-4 text-white no_line_height font-bold text-center flex mx-auto glow glow-pink">
-            PROBLEMS
-          </ScrambleText>
+          <h3>
+            <ScrambleText className="text-white no_line_height font-bold text-center flex mx-auto glow glow-pink">
+              PROBLEMS
+            </ScrambleText>
+          </h3>
           <div className="max-w-[calc(5.5*var(--space-4xl))] flex justify-center">
             <p className="text-0 text-center opacity-60">
               A fundamental Python exercise often focuses on introducing and
               reinforcing core programming concepts and syntax in the Python
-              programming language. It typically involves solving a specific
-              problem or completing a task using Python code.
+              programming language.{" "}
+              <Link href="/ske888">
+                If you are tired of coding, check out SKE888!
+              </Link>
             </p>
           </div>
         </div>
